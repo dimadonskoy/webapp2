6f28a49270bc76e550eabb2c74c78a61b1721fd8
Dmitri Donskoy <crooper22@gmail.com>
2025-07-12 12:42:39 +0300

    fixed port expose

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p><H1>Kubernetes test app version 2</H1></p>"
+    return "<H1>Kubernetes test app version 2</H1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
